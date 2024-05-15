c6d5ac1a046ffcab719c842c18fb0a68a95ba190
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-05-15 09:15:18 +0630

    learn useRef and manipulation Dom with hook

--- src/components/FormHanling.jsx
+++ src/components/FormHanling.jsx
@@ -5,7 +5,7 @@ import { useRef } from 'react';
 
 
 const FormHanling = () => {
-    const name = useRef("");
+    let name = useRef("");
     const password = useRef("");
     const message = useRef("");
 
@@ -18,10 +18,48 @@ const FormHanling = () => {
     };
 
     const forButtonType = () => {
-        console.log(name.current.value);
-        console.log(password.current.value);
-        console.log(password.current.value);
+        // console.log(name.current.value);
+        // console.log(password.current.value);
+        // console.log(password.current.value);
+        const userData = {
+            userName: name.current.value,
+            userPassword: password.current.value,
+            userMessage: message.current.value
+        };
+        // userData.userName = ""
+        console.log(userData);
+        name.current.value = "";
+        password.current.value = "";
+        message.current.value = "";
     };
+
+    function changeRed(e) {
+        // console.log("red",);
+        // console.log(e.target.style.background = "white");
+        // e.target.removeAttribute("class","bg-danger")
+        // e.target.setAttribute("class","btn btn-primary py-1 border-0")
+        e.target.parentElement.parentElement.parentElement.style.background = "red";
+        e.target.className = "btn btn-primary py-1 border-0 ";
+        
+        //****************  dom manipulation with hook  ***************
+        //I was known from that lessons one hook can manipulate overall of the DOM !!! 
+        name.current.style.border = "10px solid green";
+        password.current.style.border = "10px solid green";
+        message.current.style.border = "10px solid green";
+        name.current.focus();
+
+    }
+
+    function changeGreen(e) {
+        // console.log("green");
+        name.current.parentElement.parentElement.style.background = "green";
+        name.current.style.border = "10px solid red";
+        password.current.style.border = "10px solid red";
+        message.current.style.border = "10px solid red";
+        e.target.style.color = "white";
+        password.current.focus();
+    }
+
 
     return (
         <div className='Formhandling min-vh-100 bg-body-secondary container-fluid '>
@@ -41,7 +79,18 @@ const FormHanling = () => {
                 </div>
                 <button className='btn btn-info ms-3 px-5 py-1' type='button' onClick={() => forButtonType()}>Type button</button>
                 <button className='btn btn-info ms-3 px-5 py-1' type='submit' onClick={(e) => userInputData(e)}>Submit button</button>
+
+                {/* dom manipulation with hook */}
+                <br />
+                <div className="btn-container text-start ms-3">
+                    <div className="btn-group w-25  " style={{ marginTop: "20px" }} >
+                        <button onClick={(e) => changeRed(e)} className='btn py-1 bg-danger border-0' type='button' >change red</button>
+                        <button onClick={(e) => changeGreen(e)} className='btn py-1 bg-success border-0' type='button' >change green </button>
+                    </div>
+                </div>
+
             </form>
+
         </div>
     )
 }
